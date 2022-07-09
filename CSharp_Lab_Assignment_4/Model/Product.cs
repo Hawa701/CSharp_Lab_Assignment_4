41c1fb91c5abe521c0badb1a2e299cb0d51fdce8
Hawa701 <108014859+Hawa701@users.noreply.github.com>
2022-07-09 23:33:07 +0300

    Added the assignment

---
+++ CSharp_Lab_Assignment_4/Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Lab_Assignment_4.Model
+{
+    internal class Product
+    {
+        public int Number { get; set; }
+        public string Date { get; set; }
+        public string Inventory { get; set; }
+        public string ObjectName { get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+
+        public void save()
+        {
+            MessageBox.Show($"{ObjectName} added to inventory!");
+        }
+
+    }
+}
